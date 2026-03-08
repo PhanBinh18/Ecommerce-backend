7110092e77ea01d98ae7c0c105507c9df1645f73
pbinhf <pbinhf@gmail.com>
2026-03-08 23:06:58 +0700

    update

--- init.sql
+++ init.sql
@@ -42,3 +42,47 @@ CREATE TABLE order_items (
     -- Khóa ngoại nội bộ Module Order (An toàn)
                              FOREIGN KEY (order_id) REFERENCES orders(id)
 );
+
+INSERT INTO products
+(name, price, stock, description, image_url, category, brand, is_active)
+VALUES
+    (
+        'iPhone 15',
+        21990000,
+        15,
+        'iPhone 15 chip A16 Bionic, camera 48MP',
+        'https://res.cloudinary.com/demo/image/upload/iphone15.jpg',
+        'DIEN_THOAI',
+        'APPLE',
+        TRUE
+    ),
+    (
+        'Samsung Galaxy S24',
+        19990000,
+        10,
+        'Galaxy S24 màn hình AMOLED 120Hz',
+        'https://res.cloudinary.com/demo/image/upload/s24.jpg',
+        'DIEN_THOAI',
+        'SAMSUNG',
+        TRUE
+    ),
+    (
+        'ASUS ROG Strix G16',
+        35990000,
+        5,
+        'Laptop gaming RTX 4060, i7-13650HX',
+        'https://res.cloudinary.com/demo/image/upload/rogstrix.jpg',
+        'LAPTOP',
+        'ASUS',
+        TRUE
+    ),
+    (
+        'MacBook Air M2',
+        28990000,
+        8,
+        'MacBook Air chip Apple M2, màn hình Liquid Retina',
+        'https://res.cloudinary.com/demo/image/upload/macbookair.jpg',
+        'LAPTOP',
+        'APPLE',
+        TRUE
+    );
