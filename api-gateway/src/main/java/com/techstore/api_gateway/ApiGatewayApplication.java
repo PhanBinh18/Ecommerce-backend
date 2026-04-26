915dcb13f3a9d2e5a62ca8d2f3fbba777a6a9b52
pbinhf <pbinhf@gmail.com>
2026-04-26 21:56:39 +0700

    fix(gateway): migrate to netty webflux and setup java-based routing

--- api-gateway/src/main/java/com/techstore/api_gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/techstore/api_gateway/ApiGatewayApplication.java
@@ -2,15 +2,24 @@ package com.techstore.api_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
+	// ĐÂY CHÍNH LÀ QUYỂN SỔ CHỈ ĐƯỜNG BẰNG CODE!
+	// Trình biên dịch sẽ bảo vệ nó, không sợ sai lề như YAML nữa.
+	@Bean
+	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route("identity-service", r -> r.path("/api/auth/**", "/api/users/**")
+						.uri("lb://IDENTITY-SERVICE"))
+				.build();
+	}
 }
