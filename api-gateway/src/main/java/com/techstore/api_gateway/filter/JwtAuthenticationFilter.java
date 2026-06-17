46b35c8ed918e09916ab65df6f1774b7e95696d6
pbinhf <pbinhf@gmail.com>
2026-06-17 16:10:19 +0700

    refactor(gateway): clean up jwt filter and externalize configurations

--- api-gateway/src/main/java/com/techstore/api_gateway/filter/JwtAuthenticationFilter.java
+++ api-gateway/src/main/java/com/techstore/api_gateway/filter/JwtAuthenticationFilter.java
@@ -26,12 +26,11 @@ public class JwtAuthenticationFilter implements GlobalFilter, Ordered {
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         String path = exchange.getRequest().getURI().getPath();
 
-        // 1. CÁC ĐƯỜNG DẪN KHÔNG CẦN BẢO VỆ (ĐÃ FIX: Dùng "/auth/" để bao quát các bản v1, v2...)
         if (path.contains("/auth/") || path.contains("/swagger-ui") || path.contains("/v3/api-docs") || path.contains("/vnpay-ipn")) {
             return chain.filter(exchange);
         }
 
-        // 2. KIỂM TRA HEADER AUTHORIZATION
+        // HEADER AUTHORIZATION
         List<String> authHeaders = exchange.getRequest().getHeaders().get(HttpHeaders.AUTHORIZATION);
         if (authHeaders == null || authHeaders.isEmpty()) {
             return onError(exchange, "Missing Authorization Header", HttpStatus.UNAUTHORIZED);
@@ -42,14 +41,12 @@ public class JwtAuthenticationFilter implements GlobalFilter, Ordered {
             return onError(exchange, "Invalid Authorization format. Expected 'Bearer <token>'", HttpStatus.UNAUTHORIZED);
         }
 
-        // 3. CẮT LẤY TOKEN VÀ XÁC THỰC
         String token = authHeader.substring(7);
         try {
             byte[] keyBytes = io.jsonwebtoken.io.Decoders.BASE64.decode(secretKey);
             Key key = Keys.hmacShaKeyFor(keyBytes);
             Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
 
-            // XÁC THỰC THÀNH CÔNG -> Cho phép đi tiếp
             return chain.filter(exchange);
 
         } catch (Exception e) {
